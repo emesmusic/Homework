4c5d3c4e0c5b88b18372131b862a2fa77738059e
emesmusic@gmail.com <emesmusic@gmail.com>
2025-10-19 21:24:24 +0100

    Fixed loading animation for slow networks

--- js/71/homework.js
+++ js/71/homework.js
@@ -3,10 +3,12 @@
 
 
 (function () {
+    //I did it this way so the image was preloaded into the cache so even on a slow network it would display as soon as the button was clicked
+   const loadingAnimation = $('<img src="images/loading.gif" id="loading" alt="Loading...">'); 
     const submitButton = $('#submitButton');
     submitButton.click(
         async () => {
-            $('#fileContents').html('<img src="images/loading.gif" id="loading" alt="Loading...">');
+            $('#fileContents').html(loadingAnimation);
             try {
                 if (!$('#fileNameInput').val()) {
                     throw new Error('No valid file name provided');
