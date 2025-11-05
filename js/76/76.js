dd34680064ce72b95de27ac71bec90d236f83e14
emesmusic@gmail.com <emesmusic@gmail.com>
2025-11-05 22:58:11 +0000

    Added id based pairing instead of index based.

--- js/76/76.js
+++ js/76/76.js
@@ -8,34 +8,35 @@
     const bodyElements = document.querySelectorAll('.body-element');
     let currentDraggingItem;
     let zIndex = 0;
-
     let audioPlayed = false;
 
 
 
 
 
-    let top = 0;
-    let left = 5;
-    bodyElements.forEach(element => {
-
-        element.style.top = `${top}%`;
-        element.style.left = `${left}%`;
-
-        left += 15;
-        if (left > 80) {
-            left = 10;
-            top += 10;
-        }
-    });
 
     if (localStorage.bodyElements) {
         JSON.parse(localStorage.bodyElements).forEach(
-            (element, index) => {
-                bodyElements[index].style.top = element.top;
-                bodyElements[index].style.left = element.left;
+            element => {
+                document.getElementById(element.id).style.top = element.top;
+                document.getElementById(element.id).style.left = element.left;
             }
         );
+    }
+    else {
+        let top = 0;
+        let left = 5;
+        bodyElements.forEach(element => {
+
+            element.style.top = `${top}%`;
+            element.style.left = `${left}%`;
+
+            left += 15;
+            if (left > 80) {
+                left = 10;
+                top += 10;
+            }
+        });
     }
 
 
