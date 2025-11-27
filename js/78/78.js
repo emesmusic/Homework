95a101fbffc158e09ea2a810ec6f0f07db47ea4d
emesmusic@gmail.com <emesmusic@gmail.com>
2025-11-27 01:19:35 +0000

    Repeated without constructor.

--- js/78/78.js
+++ js/78/78.js
@@ -30,3 +30,37 @@ vehicle.print();
 const plane = new Plane('white');
 plane.go(200);
 plane.print();
+
+
+const vehiclePrototype = {
+    go(speed) {
+        this.speed = speed;
+        console.log(`Now going at speed: ${this.speed}`);
+    },
+    print() {
+        console.log('Color: ' + this.color + ', Speed: ' + this.speed);
+    }
+};
+
+function createVehicle(color) {
+const vehicle = Object.create(vehiclePrototype);
+vehicle.color = color;
+return vehicle;
+}
+
+function createPlane(color) {
+    const plane = createVehicle(color);
+    plane.go = function (speed) {
+        this.speed = speed;
+        console.log(`Now flying at speed: ${this.speed}`);
+    };
+    return plane;
+}
+
+const vehicle1 = createVehicle('blue');
+vehicle1.go(60);
+vehicle1.print();
+
+const plane1 = createPlane('green');
+plane1.go(300);
+plane1.print();
