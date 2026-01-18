303e8c8c8ceeaa05797e30c34ed7c29972872b6c
emesmusic@gmail.com <emesmusic@gmail.com>
2026-01-18 23:51:04 +0000

    Fixed formatting

--- js/93/blogReact/src/ContactUs.jsx
+++ js/93/blogReact/src/ContactUs.jsx
@@ -2,13 +2,13 @@ import React, { useState } from 'react'
 import ContactForm from './ContactForm'
 
 export default function ContactUs() {
-  const [result, setResult] = useState([]);
+  const [result, setResult] = useState(null);
   return (
-    <div className='contactus-div'>
+    <div className='contact-us-div'>
     <h1>Contact Us</h1>
     <h5>Have questions? Reach out to us!</h5>
     <ContactForm setResult={setResult} />
-      <div className={`result ${result[0] ? "success" : "fail"}`}>{result}</div>
+      <div className={`result ${result === null ? 'hidden' : `visible ${result[0] ? "success" : "fail"}`} `}>{result}</div>
     </div>
   )
 }
