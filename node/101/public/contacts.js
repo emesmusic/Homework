cf8c62449ee167b9b58d9ce6c5549cde171192c4
emesmusic@gmail.com <emesmusic@gmail.com>
2026-02-16 00:52:51 +0000

    Consolidated code so that one method deals with contacts being added from both api and browser.

--- node/101/public/contacts.js
+++ node/101/public/contacts.js
@@ -15,72 +15,46 @@
     if (!response.ok) {
       throw new Error(`${response.status} - ${response.statusText}`)
     }
-
     contactsFromApi = await response.json();
+    contactsFromApi.forEach(contact => {
+      addContact(contact);
+    });
   } catch (error) {
     document.getElementById('error-box').innerText = error;
-  }
-
-  if (contactsFromApi.length > 0) {
-    if (!contacts.length) {
-      contactsTable.deleteRow(0);
-    }
-    contactsFromApi.forEach(contact => {
-      const newContact = {
-        first: contact.first,
-        last: contact.last,
-        email: contact.email,
-        phone: contact.phone,
-      };
-      contacts.push(newContact);
-      const row = contactsTable.insertRow();
-      row.innerHTML = `<td>${newContact.first}</td>
-                     <td>${newContact.last}</td>
-                     <td>${newContact.email}</td>
-                     <td>${newContact.phone}</td>`;
-    });
   }
 
 
   addContactForm.addEventListener('submit', e => {
     e.preventDefault();
+    addContact();
+  });
 
+  function addContact(contact) {
     if (!contacts.length) {
       contactsTable.deleteRow(0);
     }
 
-    const newContact = {
-      first: firstInput.value,
-      last: lastInput.value,
-      email: emailInput.value,
-      phone: phoneInput.value,
-    };
-
+    let newContact;
+    if (contact) {
+      newContact = contact;
+    }
+    else {
+      newContact = {
+        first: firstInput.value,
+        last: lastInput.value,
+        email: emailInput.value,
+        phone: phoneInput.value,
+      };
+      addContactForm.reset();
+    }
     contacts.push(newContact);
-
-    /*const newRow = document.createElement('tr');
-    const firstCell = document.createElement('td');
-    firstCell.innerText = 'Donald';
-    newRow.appendChild(firstCell);
-    contactsTable.appendChild(newRow);*/
-
     const row = contactsTable.insertRow();
-    /*const first = row.insertCell();
-    first.innerText = 'Donald';
-    const last = row.insertCell();
-    last.innerText = 'Trump';*/
-
     row.innerHTML = `<td>${newContact.first}</td>
                      <td>${newContact.last}</td>
                      <td>${newContact.email}</td>
                      <td>${newContact.phone}</td>`;
+  }
 
-    /*firstInput.value = '';
-    lastInput.value = '';
-    emailInput.value = '';
-    phoneInput.value = '';*/
 
-    addContactForm.reset();
-  });
 
 }());
