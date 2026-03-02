ab89848c56d3b441f15ea01767f82b6b44e3c425
emesmusic@gmail.com <emesmusic@gmail.com>
2026-03-02 02:23:28 +0000

    Homework

---
+++ node/104/routes/index.js
@@ -0,0 +1,44 @@
+var express = require('express');
+var router = express.Router();
+var session = require('express-session')
+
+
+
+router.use(session({
+  secret: 'password',
+  resave: false,
+  saveUninitialized: false
+})
+
+
+
+)
+
+
+/* GET home page. */
+router.get('/', function (req, res, next) {
+  if(req.session.views){
+    ++req.session.views;
+  }
+  else{
+    req.session.views = 1;
+  }
+  
+
+
+  res.render('index', { title: 'Cookies', views: req.session.views });
+});
+
+
+router.get('/name-page', (req, res) => {
+  req.session.name = req.query.name || req.session.name || null;
+
+  
+  res.render('namePage', { title: 'Name Page', name: req.session.name })
+})
+
+
+
+
+
+module.exports = router;
